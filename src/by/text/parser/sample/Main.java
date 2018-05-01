c41e9b546f83fb3efd9ae718d4c772af23d6a8b5
Kate Vygonnaya <katya.vygonnaya@gmail.com>
2018-05-01 15:31:51 +0300

    jar file was created

--- src/by/text/parser/sample/Main.java
+++ src/by/text/parser/sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("../resources/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Scene scene = new Scene(root, 770, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
